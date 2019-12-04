5e5260710a946f5686029985b55ebcdcda0bc177
Scott Abbey <scott@eotr.net>
2019-12-03 22:10:30 -0500

    day01-2 complete

--- cmd/day01/part1.go
+++ cmd/day01/part1.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 )
 
-func getModuleWeights() []int {
+func getModuleMasses() []int {
 	file, err := os.Open("assets/day01input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -16,31 +16,42 @@ func getModuleWeights() []int {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-	weights := make([]int, 0, 10)
+	masses := make([]int, 0, 10)
 	for scanner.Scan() {
 		number, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			log.Fatal(err)
 		}
-		weights = append(weights, number)
+		masses = append(masses, number)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	return weights
+	return masses
+}
+
+func calculateFuel(masses []int) []int {
+	fuelLoads := make([]int, 0, 10)
+
+	for _, mass := range masses {
+		fuelLoads = append(fuelLoads, (mass/3)-2)
+	}
+
+	return fuelLoads
 }
 
 func part1() {
-	fuelSubtotals := make([]int, 0, 10)
 	fuelTotal := 0
 
-	weights := getModuleWeights()
+	masses := getModuleMasses()
 
-	for _, subtotal := range subtotals {
-		total += subtotal
+	fuelLoads := calculateFuel(masses)
+
+	for _, fuelLoad := range fuelLoads {
+		fuelTotal += fuelLoad
 	}
 
-	fmt.Println(total)
+	fmt.Println(fuelTotal)
 }
--- cmd/day01/part2.go
+++ cmd/day01/part2.go
@@ -1,5 +1,23 @@
 package main
 
-func part2() {
+import "fmt"
 
+func calculateMoreFuel(thisFuel int, totalFuel int) int {
+	if thisFuel/3 <= 2 {
+		return totalFuel
+	}
+	newFuel := (thisFuel / 3) - 2
+
+	return calculateMoreFuel(newFuel, (newFuel + totalFuel))
+}
+
+func part2() {
+	masses := getModuleMasses()
+	fuelLoads := calculateFuel(masses)
+	newTotal := 0
+	for _, fuelLoad := range fuelLoads {
+		newTotal += calculateMoreFuel(fuelLoad, fuelLoad)
+	}
+
+	fmt.Println(newTotal)
 }
